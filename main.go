975e0a43b90e4f7dce0e3a7a6e01055a5b2ea8d1
bmason006 <bmason006@student.butte.edu>
2020-02-05 03:57:24 +0000

    Added art, current weather and current date

---
+++ main.go
@@ -0,0 +1,29 @@
+// Programmer name: Brooke Mason
+// Date completed:  1/30/20
+// Description:Lab 1
+
+package main
+
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("My name is Brooke, and my major is Computer Science.")
+	fmt.Println("This is my fourth semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is making art.")
+
+  fmt.Println("H     H  EEEEE L     L    OOOOO")
+  fmt.Println("H     H  E     L     L    O   O")
+  fmt.Println("HHHHHHH  EEEEE L     L    O   O")
+  fmt.Println("H     H  E     L     L    O   O")
+  fmt.Println("H     H  EEEEE LLLL  LLLL OOOOO")
+
+ 
+  fmt.Println("Chico California, 32.6 Degrees currently")
+  fmt.Println("Tomorrows high temp will be 54.3 degrees")
+
+  fmt.Println("Today is Febuary 4th 2020.")
+  fmt.Println("02-04-2020")
+}
+
+
